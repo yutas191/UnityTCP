a4a7bfd192def9f8cf8fc3000d2fb6363d1c07f4
yutas191 <yutas191@gmail.com>
2019-05-03 01:13:58 +0900

    Add UnityTCP

---
+++ TcpClient/Assets/TcpSender.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class TcpSender : MonoBehaviour {
+
+	private TcpClient client = null;
+	private NetworkStream Stream;
+	private string ipString = "127.0.0.1";
+	private int port = 10021;
+	private int count = 0;
+	private string text;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			text = "Hello:" + (count++);
+			Debug.Log ("SEND:"+text);
+			client = new TcpClient(ipString, port);
+			Stream = client.GetStream ();
+			byte[] msg = System.Text.Encoding.ASCII.GetBytes (text);
+			Stream.Write (msg, 0, msg.Length);
+			Stream.Flush ();
+			Stream.Close ();
+			client.Close ();
+		}
+	}
+}
---
+++ TcpServer/Assets/TcpReceiver.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using UnityEngine;
+
+public class TcpReceiver : MonoBehaviour {
+	private TcpListener server;
+	private byte[] msg = new byte[256];
+	private int i;
+	private string text = null;
+
+	private Thread thread;
+
+	// Use this for initialization
+	void Start () {
+		thread = new Thread (ThreadReceive);
+		thread.Start ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void ThreadReceive()
+	{
+		Debug.Log("START");
+
+		string ipString = "127.0.0.1";
+		System.Net.IPAddress ipAdd = System.Net.IPAddress.Parse(ipString);
+		int port = 10021;
+
+		server = new TcpListener(ipAdd, port);
+
+		server.Start();
+		Debug.Log("Listenを開始しました "+
+			((System.Net.IPEndPoint)server.LocalEndpoint).Address+":"+
+			((System.Net.IPEndPoint)server.LocalEndpoint).Port);
+		
+		while (true) {
+			TcpClient client = server.AcceptTcpClient ();
+			NetworkStream stream = client.GetStream ();
+			while ((i = stream.Read (msg, 0, msg.Length)) != 0) {
+				text = System.Text.Encoding.ASCII.GetString (msg, 0, i);
+			}
+			Debug.Log ("RECEIVE:" + text);
+			client.Close ();
+		}
+	}
+}
